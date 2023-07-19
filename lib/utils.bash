510c42c61626a5e09c0176b35325b3bac3744085
Phil Brocker <phil.brocker@outreach.io>
2023-07-19 12:39:45 -0400

    update download

--- lib/utils.bash
+++ lib/utils.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for cloud-sql-proxy.
 GH_REPO="https://github.com/GoogleCloudPlatform/cloud-sql-proxy"
-TOOL_NAME="cloud-sql-proxy"
+TOOL_NAME="cloud_sql_proxy"
 TOOL_TEST="cloud_sql_proxy --version"
 
 fail() {
@@ -15,9 +15,9 @@ fail() {
 curl_opts=(-fsSL)
 
 # NOTE: You might want to remove this if cloud-sql-proxy is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+# if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+#   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+# fi
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
@@ -36,13 +36,37 @@ list_all_versions() {
   list_github_tags
 }
 
+get_platform() {
+  local os=$(uname)
+  if [[ "${os}" == "Darwin" ]]; then
+    echo "darwin"
+  elif [[ "${os}" == "Linux" ]]; then
+    echo "linux"
+  else
+    echo >&2 "unsupported os: ${os}" && exit 1
+  fi
+}
+
+get_arch() {
+  local os=$(uname)
+  local arch=$(uname -m)
+  if [[ "${os}" == "Darwin" && "${arch}" == "arm64" ]]; then
+    echo "arm64"
+  elif [[ "${os}" == "Linux" && "${arch}" == "aarch64" ]]; then
+    echo "aarch_64"
+  elif [[ ("${os}" == "Linux" || "${os}" == "Darwin") && "${arch}" == "x86_64" ]]; then
+    echo "amd64"
+  else
+    echo "${arch}"
+  fi
+}
+
 download_release() {
   local version filename url
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for cloud-sql-proxy
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v${version}/cloud-sql-proxy.$(get_platform).$(get_arch)"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -59,7 +83,8 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp "$ASDF_DOWNLOAD_PATH/$TOOL_NAME" "$install_path/$TOOL_NAME"
+    chmod +x "$install_path/$TOOL_NAME"
 
     # TODO: Assert cloud-sql-proxy executable exists.
     local tool_cmd
